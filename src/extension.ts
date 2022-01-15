5a385d1a952219790ee3eed3925c2f819786dd3a
Stefan Grandner <stefan.grandner@stayfriends.com>
2022-01-15 02:29:04 +0100

    change selected text to upper case

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,42 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "upper-snake-case" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('upper-snake-case.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hallo Welt von Upper Snake Case!');
+
+        const activeTextEditor = vscode.window.activeTextEditor;
+
+        if (!!activeTextEditor) {
+
+            const selection = activeTextEditor.selection;
+
+            if (!!selection) {
+
+                const range = new vscode.Range(selection.start, selection.end);
+
+                const selectedText = activeTextEditor.document.getText(range);
+
+                if (range.isEmpty || selectedText.length === 0) {
+                    vscode.window.showWarningMessage('nothing selected');
+                }
+
+                vscode.window.showInformationMessage(selectedText);
+
+                activeTextEditor.edit((editBuilder: vscode.TextEditorEdit) => {
+
+                    editBuilder.replace(selection, selectedText.toUpperCase());
+
+                }).then((editDone: boolean) => {
+                    vscode.window.showInformationMessage(`edit done: ${editDone}`);
+                });
+            }
+        }
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+}
