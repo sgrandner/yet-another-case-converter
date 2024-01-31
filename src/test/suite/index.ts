a14e800b979a327597cc9219e453c83c186a4dd0
Stefan Grandner <sgrandner@gmx.de>
2024-01-31 23:55:16 +0100

    add log to mocha test suite which test files are added

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -19,7 +19,10 @@ export function run(): Promise<void> {
             }
 
             // Add files to the test suite
-            files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+            files.forEach(f => {
+                console.log(`add test file ${f} to mocha run`);
+                mocha.addFile(path.resolve(testsRoot, f));
+            });
 
             try {
                 // Run the mocha test
